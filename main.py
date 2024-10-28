bdf6ec5634d46cfb50080f07615b5532c9eac50c
=nikosgri <=n.grigoriadis09@gmail.com>
2024-10-28 20:35:40 +0200

    Feat: Top bar menu, minimize & close

--- main.py
+++ main.py
@@ -3,27 +3,36 @@ from kivy.lang import Builder
 from kivy.core.window import Window
 from kivy.utils import get_color_from_hex
 from kivy.core.text import LabelBase
-from kivy.uix.button import ButtonBehavior, Button
+from kivy.uix.button import ButtonBehavior,Button
 from kivy.uix.image import Image
 from kivy.uix.label import Label
-from kivy.uix.screenmanager import ScreenManager, Screen, NoTransition, CardTransition
+from kivy.uix.screenmanager import ScreenManager,Screen,NoTransition,CardTransition
 from kivy.config import Config
 from datetime import date
 from covid import Covid
 from kivy.uix.boxlayout import BoxLayout
 
 
-# Kivy window configuration
-Config.set('graphics', 'width', '1200')
-Config.set('graphics', 'height', '900')
-Config.set('graphics', 'borderless', '1')
+
+Config.set('graphics','width','1200')
+Config.set('graphics', 'height','900')
+Config.set('graphics','borderless','1')
 Config.write()
 
+class ImageButton(ButtonBehavior, Image):
+    pass
+
+class LabelButton(ButtonBehavior, Label):
+    pass
+
+class Spacer(Label):
+    pass
 
 class DashboardScreen(Screen):
     pass
 
-GUI  = Builder.load_file("main.kv")
+
+GUI = Builder.load_file("main.kv") # Load the main file to builder
 
 class MainApp(App):
 
@@ -32,5 +41,11 @@ class MainApp(App):
     
     def on_start(self):
         pass
+    
+    def close(self):
+        quit()
+    
+    def minimize(self):
+        Window.minimize()
 
-MainApp().run
+MainApp().run()
